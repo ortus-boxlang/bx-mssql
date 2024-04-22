6aa2301e20a67f0c5ff88ea6616c463f0ee58c9f
Luis Majano <lmajano@gmail.com>
2024-04-22 16:45:37 +0200

    finalized

---
+++ src/main/java/ortus/boxlang/modules/bxmssql/MicrosoftSQLDriver.java
@@ -0,0 +1,90 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ortus.boxlang.modules.bxmssql;
+
+import ortus.boxlang.runtime.config.segments.DatasourceConfig;
+import ortus.boxlang.runtime.dynamic.casters.StringCaster;
+import ortus.boxlang.runtime.jdbc.drivers.DatabaseDriverType;
+import ortus.boxlang.runtime.jdbc.drivers.GenericJDBCDriver;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.IStruct;
+import ortus.boxlang.runtime.types.Struct;
+
+/**
+ * The Microsoft SQL JDBC Driver
+ * https://learn.microsoft.com/en-us/sql/connect/jdbc/building-the-connection-url?view=sql-server-ver16
+ */
+public class MicrosoftSQLDriver extends GenericJDBCDriver {
+
+	protected static final String	DEFAULT_CLASSNAME			= "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	protected static final String	DEFAULT_PORT				= "1433";
+	protected static final String	DEFAULT_HOST				= "localhost";
+	protected static final String	DEFAULT_DELIMITER			= ";";
+	protected static final IStruct	DEFAULT_HIKARI_PROPERTIES	= Struct.of();
+	protected static final IStruct	DEFAULT_CUSTOM_PARAMS		= Struct.of();
+
+	/**
+	 * Constructor
+	 */
+	public MicrosoftSQLDriver() {
+		super();
+		this.name					= new Key( "MicrosoftSQL" );
+		this.type					= DatabaseDriverType.MSSQL;
+		this.driverClassName		= DEFAULT_CLASSNAME;
+		this.defaultDelimiter		= DEFAULT_DELIMITER;
+		this.defaultCustomParams	= DEFAULT_CUSTOM_PARAMS;
+		this.defaultProperties		= DEFAULT_HIKARI_PROPERTIES;
+	}
+
+	@Override
+	public String buildConnectionURL( DatasourceConfig config ) {
+		// Validate the host
+		String host = ( String ) config.properties.getOrDefault( "host", DEFAULT_HOST );
+		if ( host.isEmpty() ) {
+			host = DEFAULT_HOST;
+		}
+
+		// Instance Name : Optional
+		String instanceName = ( String ) config.properties.getOrDefault( "instanceName", "" );
+		if ( !instanceName.isEmpty() ) {
+			host = host + "\\" + instanceName;
+		}
+
+		// Port
+		String port = StringCaster.cast( config.properties.getOrDefault( "port", DEFAULT_PORT ) );
+		if ( port.isEmpty() || port.equals( "0" ) ) {
+			port = DEFAULT_PORT;
+		}
+
+		// Validate the database
+		String database = ( String ) config.properties.getOrDefault( "database", "" );
+		if ( database.isEmpty() ) {
+			throw new IllegalArgumentException( "The database property is required for the Microsoft JDBC Driver" );
+		}
+
+		// Build the connection URL with no host info
+		return String.format(
+		    "jdbc:sqlserver://%s:%s;databaseName=%s;%s",
+		    host,
+		    port,
+		    database,
+		    customParamsToQueryString( config )
+		);
+	}
+
+}
---
+++ src/test/java/ortus/boxlang/modules/bxmssql/MicrosoftSQLDriverTest.java
@@ -0,0 +1,54 @@
+package ortus.boxlang.modules.bxmssql;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.config.segments.DatasourceConfig;
+import ortus.boxlang.runtime.jdbc.drivers.DatabaseDriverType;
+import ortus.boxlang.runtime.scopes.Key;
+
+public class MicrosoftSQLDriverTest {
+
+	@Test
+	@DisplayName( "Test getName()" )
+	public void testGetName() {
+		MicrosoftSQLDriver	driver			= new MicrosoftSQLDriver();
+		Key					expectedName	= new Key( "MicrosoftSQL" );
+		assertThat( driver.getName() ).isEqualTo( expectedName );
+	}
+
+	@Test
+	@DisplayName( "Test getType()" )
+	public void testGetType() {
+		MicrosoftSQLDriver	driver			= new MicrosoftSQLDriver();
+		DatabaseDriverType	expectedType	= DatabaseDriverType.MSSQL;
+		assertThat( driver.getType() ).isEqualTo( expectedType );
+	}
+
+	@Test
+	@DisplayName( "Test buildConnectionURL()" )
+	public void testBuildConnectionURL() {
+		MicrosoftSQLDriver	driver	= new MicrosoftSQLDriver();
+		DatasourceConfig	config	= new DatasourceConfig();
+		config.properties.put( "driver", "postgresql" );
+		config.properties.put( "database", "mydb" );
+
+		String expectedURL = "jdbc:sqlserver://localhost:1433;databaseName=mydb;";
+		assertThat( driver.buildConnectionURL( config ) ).isEqualTo( expectedURL );
+	}
+
+	@DisplayName( "Throw an exception if the database is not found" )
+	@Test
+	public void testBuildConnectionURLNoDatabase() {
+		MicrosoftSQLDriver	driver	= new MicrosoftSQLDriver();
+		DatasourceConfig	config	= new DatasourceConfig();
+
+		assertThrows( IllegalArgumentException.class, () -> {
+			driver.buildConnectionURL( config );
+		} );
+	}
+
+}
