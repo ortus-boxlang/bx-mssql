5d10dc32df10bbd122a9e426863feb3310706c8f
Luis Majano <lmajano@gmail.com>
2024-04-23 18:03:49 -0400

    package updates

--- src/main/java/ortus/boxlang/modules/mssql/MicrosoftSQLDriver.java
+++ src/main/java/ortus/boxlang/modules/mssql/MicrosoftSQLDriver.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ortus.boxlang.modules.bxmssql;
+package ortus.boxlang.modules.mssql;
 
 import ortus.boxlang.runtime.config.segments.DatasourceConfig;
 import ortus.boxlang.runtime.dynamic.casters.StringCaster;
